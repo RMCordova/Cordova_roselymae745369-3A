39cf66b193d2864f82e7c0f088e8fe8e03c85e9f
RMCordova <cordovaroselymae@gmail.com>
2023-04-27 10:19:27 +0800

    4th commit

---
+++ src/components/form.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap/Button';
+import { Form } from 'react-bootstrap/Form';
+
+function BasicForm()
+{
+    const fields =[
+        {label: 'Email Address', type: 'text', placeholder: 'Input Email'},
+        {label: 'Password', type: 'password', placeholder: 'Password'}
+    ] 
+}
+
+return (
+    <Form>
+        
+    </Form>
+)
