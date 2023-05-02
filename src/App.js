f94b569764cfe5192b967d1375395f2478f1d8e2
RMCordova <cordovaroselymae@gmail.com>
2023-05-02 08:43:59 +0800

    7th commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
-import { BrowserRouter as router, Routes, Route, Router } from 'react-router-dom';
-import Navbar from './Nav';
-import Login from './LogIn';
-import Registration from './Registration';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './pages/Login.components'
+import SignUp from './pages/Signup.components'
 
-function App()
-{
+function App() {
   return (
     <Router>
-      <Navbar />
-        <Routes>
-          <Route path='Nav' element= {<Navbar />} />
-          <Route path='LogIn' element= {<Login />} />
-          <Route path='Registration' element= {<Registration />} />
-        </Routes>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/home'}>
+              Home Page
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/log-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path='' element= {<></>} />
+              <Route path="/log-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </Router>
   )
 }
-
-export default App;
+export default App
